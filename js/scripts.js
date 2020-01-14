96b7fc3aa4a9b738fd6034bf627a1bd88b99016e
Erik Grove and Krista Rutz <erik@>
2020-01-14 15:18:28 -0800

    include image and add a click function to h1

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
+function gnomer(){
+  for (var i=0; i<200000; i++) {
+  $(".btn-danger").after('<img class="removable" src="walrus.jpg">');
+  }
+  alert("GNOME ATTACK");
+}
+
 $(document).ready(function() {
-  $("button#green").click(function(){
+  $("#green").click(function(){
     $("body").removeClass();
     $("body").addClass("green-background");
   });
@@ -11,11 +18,18 @@ $(document).ready(function() {
 
   $("button#red").click(function(){
     $("body").removeClass();
+    $(".removable").remove();
     $("body").addClass("red-background");
   });
 
   $("button#default").click(function(){
     $("body").removeClass();
-  })
-});
+  });
 
+  $("#salad").click(function(){
+    $("body").removeClass();
+    $(".removable").remove();
+    $("#salad").after('<p class="removable" >Don\'t click me, silly!</p>');
+    $("#salad").after('<img class="removable" src="walrus.jpg">');
+  });
+});
